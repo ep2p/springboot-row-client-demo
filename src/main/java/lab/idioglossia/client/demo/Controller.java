7ac5843ccf994572cbdfcad2b0abc8450d1aa858
sepehr <sepehrgh01@gmail.com>
2020-09-09 11:41:19 +0430

    Using latest row-client-starter

--- src/main/java/lab/idioglossia/client/demo/Controller.java
+++ src/main/java/lab/idioglossia/client/demo/Controller.java
@@ -1,7 +1,8 @@
 package lab.idioglossia.client.demo;
 
 import lab.idioglossia.row.client.RowClient;
-import lab.idioglossia.row.client.callback.HttpExtendedResponseCallback;
+import lab.idioglossia.row.client.RowClientFactory;
+import lab.idioglossia.row.client.callback.ResponseCallback;
 import lab.idioglossia.row.client.model.RowRequest;
 import lab.idioglossia.row.client.model.RowResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +14,12 @@ import java.io.IOException;
 @RestController
 public class Controller {
     private final RowClient rowClient;
+    private final RowClientFactory rowClientFactory;
 
     @Autowired
-    public Controller(RowClient rowClient) {
+    public Controller(RowClient rowClient, RowClientFactory rowClientFactory) {
         this.rowClient = rowClient;
+        this.rowClientFactory = rowClientFactory;
     }
 
     @GetMapping("/send/1")
@@ -25,12 +28,28 @@ public class Controller {
                 .address("/t1")
                 .method(RowRequest.RowMethod.GET)
                 .build();
-        rowClient.sendRequest(request, new HttpExtendedResponseCallback<SampleDto>() {
+        rowClient.sendRequest(request, new ResponseCallback<SampleDto>(SampleDto.class) {
             @Override
-            public Class<SampleDto> getResponseBodyClass() {
-                return SampleDto.class;
+            public void onResponse(RowResponse<SampleDto> rowResponse) {
+                System.out.println(rowResponse.getBody().toString());
             }
 
+            @Override
+            public void onError(Throwable throwable) {
+                throwable.printStackTrace();
+            }
+        });
+    }
+
+    @GetMapping("/send/2")
+    public void send2() throws IOException {
+        RowClient rowClient = rowClientFactory.getRowClient("ws://localhost:8080/ws");
+        rowClient.open();
+        RowRequest<SampleDto, SampleDto> request = RowRequest.<SampleDto, SampleDto>builder()
+                .address("/t1")
+                .method(RowRequest.RowMethod.GET)
+                .build();
+        rowClient.sendRequest(request, new ResponseCallback<SampleDto>(SampleDto.class) {
             @Override
             public void onResponse(RowResponse<SampleDto> rowResponse) {
                 System.out.println(rowResponse.getBody().toString());
