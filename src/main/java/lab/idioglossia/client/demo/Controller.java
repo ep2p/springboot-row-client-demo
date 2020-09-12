4767af56f415ace3f3f97080f28b74dddd8494e4
sepehr <sepehrgh01@gmail.com>
2020-09-12 11:03:05 +0430

    Added rest of the methods from server demo

--- src/main/java/lab/idioglossia/client/demo/Controller.java
+++ src/main/java/lab/idioglossia/client/demo/Controller.java
@@ -2,7 +2,9 @@ package lab.idioglossia.client.demo;
 
 import lab.idioglossia.row.client.RowClient;
 import lab.idioglossia.row.client.RowClientFactory;
+import lab.idioglossia.row.client.Subscription;
 import lab.idioglossia.row.client.callback.ResponseCallback;
+import lab.idioglossia.row.client.callback.SubscriptionListener;
 import lab.idioglossia.row.client.model.RowRequest;
 import lab.idioglossia.row.client.model.RowResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.UUID;
 
 @RestController
 public class Controller {
@@ -22,6 +25,7 @@ public class Controller {
         this.rowClientFactory = rowClientFactory;
     }
 
+    //t1
     @GetMapping("/send/1")
     public void send1() throws IOException {
         RowRequest<SampleDto, SampleDto> request = RowRequest.<SampleDto, SampleDto>builder()
@@ -31,7 +35,7 @@ public class Controller {
         rowClient.sendRequest(request, new ResponseCallback<SampleDto>(SampleDto.class) {
             @Override
             public void onResponse(RowResponse<SampleDto> rowResponse) {
-                System.out.println(rowResponse.getBody().toString());
+                System.out.println(rowResponse.toString());
             }
 
             @Override
@@ -41,8 +45,121 @@ public class Controller {
         });
     }
 
+    //t2
     @GetMapping("/send/2")
     public void send2() throws IOException {
+        RowRequest<SampleDto, SampleDto> request = RowRequest.<SampleDto, SampleDto>builder()
+                .address("/t2")
+                .method(RowRequest.RowMethod.POST)
+                .body(new SampleDto("alter me :P "))
+                .build();
+        rowClient.sendRequest(request, new ResponseCallback<SampleDto>(SampleDto.class) {
+            @Override
+            public void onResponse(RowResponse<SampleDto> rowResponse) {
+                System.out.println(rowResponse.toString());
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                throwable.printStackTrace();
+            }
+        });
+    }
+
+    //t3
+    @GetMapping("/send/3")
+    public void send3() throws IOException {
+        RowRequest<SampleDto, SampleDto> request = RowRequest.<SampleDto, SampleDto>builder()
+                .address("/t3")
+                .method(RowRequest.RowMethod.POST)
+                .body(new SampleDto("alter me :P "))
+                .query(new SampleDto("This is my query"))
+                .build();
+        rowClient.sendRequest(request, new ResponseCallback<SampleDto>(SampleDto.class) {
+            @Override
+            public void onResponse(RowResponse<SampleDto> rowResponse) {
+                System.out.println(rowResponse.toString());
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                throwable.printStackTrace();
+            }
+        });
+    }
+
+    //t4
+    @GetMapping("/send/4")
+    public void send4() throws IOException {
+        RowRequest<SampleDto, SampleDto> request = RowRequest.<SampleDto, SampleDto>builder()
+                .address("/t4/hello")
+                .method(RowRequest.RowMethod.GET)
+                .build();
+        rowClient.sendRequest(request, new ResponseCallback<SampleDto>(SampleDto.class) {
+            @Override
+            public void onResponse(RowResponse<SampleDto> rowResponse) {
+                System.out.println(rowResponse.toString());
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                throwable.printStackTrace();
+            }
+        });
+    }
+
+    //subscribe
+    @GetMapping("/send/5")
+    public void send5() throws IOException {
+        RowRequest<SampleDto, SampleDto> request = RowRequest.<SampleDto, SampleDto>builder()
+                .address("/subs/t1")
+                .method(RowRequest.RowMethod.GET)
+                .build();
+
+        rowClient.subscribe(request, new ResponseCallback<SampleDto>(SampleDto.class) {
+            @Override
+            public void onResponse(RowResponse<SampleDto> rowResponse) {
+                System.out.println(rowResponse);
+                System.out.println(rowResponse.getSubscription());
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                throwable.printStackTrace();
+            }
+        }, new SubscriptionListener<SampleDto>(SampleDto.class) {
+            @Override
+            public void onMessage(Subscription subscription, SampleDto sampleDto) {
+                System.out.println("Received published message-> " + sampleDto);
+                System.out.println(subscription);
+            }
+        });
+    }
+
+    //publish
+    @GetMapping("/send/6")
+    public void send6() throws IOException {
+        RowRequest<SampleDto, SampleDto> request = RowRequest.<SampleDto, SampleDto>builder()
+                .address("/subs/publish/t1")
+                .method(RowRequest.RowMethod.POST)
+                .body(new SampleDto("A published messaged with id: "+ UUID.randomUUID().toString()))
+                .build();
+        rowClient.sendRequest(request, new ResponseCallback<SampleDto>(SampleDto.class) {
+            @Override
+            public void onResponse(RowResponse<SampleDto> rowResponse) {
+                System.out.println(rowResponse.toString());
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                throwable.printStackTrace();
+            }
+        });
+    }
+
+    //Example to create new row client from factory
+    @GetMapping("/send/n")
+    public void sendN() throws IOException {
         RowClient rowClient = rowClientFactory.getRowClient("ws://localhost:8080/ws");
         rowClient.open();
         RowRequest<SampleDto, SampleDto> request = RowRequest.<SampleDto, SampleDto>builder()
